cbee053a77a5912bc3fb9f57fd53bd08b34eddda
HEDI <admin@otamoon.hu>
2025-11-23 20:51:07 +0100

    Update favicon and touch icon assets and links
    Replaced favicon and apple-touch-icon image files with updated versions and adjusted the HTML link tags in root.tsx for improved icon support and consistency across platforms.

--- app/root.tsx
+++ app/root.tsx
@@ -44,12 +44,10 @@ export const links: LinksFunction = () => [
   { rel: 'dns-prefetch', href: 'https://fonts.gstatic.com' },
 
   // Favicon and touch icons
-  { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
   { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-  { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
-  { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/web-app-manifest-192x192.png' },
-  { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/web-app-manifest-512x512.png' },
-  { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+  { rel: 'icon', type: 'shortcut icon', href: '/favicon.ico' },
+  { rel: 'icon', type: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
   { rel: 'manifest', href: '/site.webmanifest' },
 ];
 
